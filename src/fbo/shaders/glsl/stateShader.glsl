53d8cb16ffa497feacf120cabd3ec20ab9367c64
zhekeani <zhekenai@gmail.com>
2024-09-20 15:55:43 +0700

    Change angle('x') mapping logic in state shader

--- src/fbo/shaders/glsl/stateShader.glsl
+++ src/fbo/shaders/glsl/stateShader.glsl
@@ -3,16 +3,19 @@ precision highp sampler2D;
 
 varying vec2 vUv;
 uniform float maxY;
+uniform bool isLarge;
 
 void main () {
     const float PI = 3.1416;
 
-    // Use mix to map vUv.x from [0, 1] to [-PI, PI]
-    float xMapped = mix(-PI, PI, vUv.x);
+    float xMin = -PI;
+    float xMax = isLarge ? 3.0 * PI : PI;
 
-    // Use mix to map vUv.y from [0, 1] to [-maxY, maxY]
+    // Map vUv.x from [0, 1] to [xMin, xMax]
+    float xMapped = mix(xMin, xMax, vUv.x);
+
+    // Map vUv.y from [0, 1] to [-maxY, maxY]
     float yMapped = mix(-maxY, maxY, vUv.y);
 
-    // Output the mapped coordinates (for visualization or further processing)
     gl_FragColor = vec4(xMapped, yMapped, 0.0, 1.0); 
 }
