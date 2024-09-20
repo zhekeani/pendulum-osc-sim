dc6eda37e9c6039d999c7d32f1dd3ad828e11b59
zhekeani <zhekenai@gmail.com>
2024-09-20 17:30:09 +0700

    Correct angle('x') mapping in state shader

--- src/fbo/shaders/glsl/stateShader.glsl
+++ src/fbo/shaders/glsl/stateShader.glsl
@@ -9,13 +9,14 @@ void main () {
     const float PI = 3.1416;
 
     float xMin = -PI;
-    float xMax = isLarge ? 3.0 * PI : PI;
+    float xMax = isLarge ? 2.0 * PI : PI;
+    float yMax = isLarge ? maxY * 0.5 : maxY;
 
     // Map vUv.x from [0, 1] to [xMin, xMax]
     float xMapped = mix(xMin, xMax, vUv.x);
 
     // Map vUv.y from [0, 1] to [-maxY, maxY]
-    float yMapped = mix(-maxY, maxY, vUv.y);
+    float yMapped = mix(-maxY, yMax, vUv.y);
 
     gl_FragColor = vec4(xMapped, yMapped, 0.0, 1.0); 
 }
