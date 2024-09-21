4fd68aa66e7ca047a8581723813d59d96c475293
zhekeani <zhekenai@gmail.com>
2024-09-21 16:46:17 +0700

    Fix oscillation state calculation logic

--- src/fbo/shaders/glsl/stateShader.glsl
+++ src/fbo/shaders/glsl/stateShader.glsl
@@ -3,20 +3,20 @@ precision highp sampler2D;
 
 varying vec2 vUv;
 uniform float maxY;
-uniform bool isLarge;
+uniform float loopCount;
 
 void main () {
     const float PI = 3.1416;
+    vec2 uv = vUv.xy;
 
     float xMin = -PI;
-    float xMax = isLarge ? 2.0 * PI : PI;
-    float yMax = isLarge ? maxY * 0.5 : maxY;
+    float xMax =   PI * 2.0 * loopCount - PI;
 
     // Map vUv.x from [0, 1] to [xMin, xMax]
-    float xMapped = mix(xMin, xMax, vUv.x);
+    float xMapped = mix(xMin, xMax, uv.x);
 
     // Map vUv.y from [0, 1] to [-maxY, maxY]
-    float yMapped = mix(-maxY, yMax, vUv.y);
+    float yMapped = mix(-maxY, maxY , uv.y);
 
     gl_FragColor = vec4(xMapped, yMapped, 0.0, 1.0); 
 }
