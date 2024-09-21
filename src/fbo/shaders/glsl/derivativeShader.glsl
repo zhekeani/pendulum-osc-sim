7c693d51c5aba204c5cec767852b9598b4891ad8
zhekeani <zhekenai@gmail.com>
2024-09-21 16:54:02 +0700

    Finish updates on update branch to accommodate large canvas's size

--- src/fbo/shaders/glsl/derivativeShader.glsl
+++ src/fbo/shaders/glsl/derivativeShader.glsl
@@ -1,22 +1,20 @@
 precision highp float;
 precision highp sampler2D;
 
-varying vec2 vUv;  // UV coordinates for texture sampling
-uniform sampler2D stateTexture; // Texture that holds the current state [theta, thetaDot]
-uniform float g;    // Gravitational constant
-uniform float L;    // Length of the pendulum
+varying vec2 vUv;  
+uniform sampler2D stateTexture; 
+uniform float g;
+uniform float L;    
+uniform float mu;
 
 void main() {
-    // Fetch the current state from the texture
     vec4 currentState = texture2D(stateTexture, vUv);
 
-    // Extract the current angle (theta) and angular velocity (thetaDot)
-    float theta = currentState.r;      // The angle theta(t)
+    float theta = currentState.r;
     float thetaDot = currentState.g;
     
     float dTheta_dt = thetaDot;
-    float dThetaDot_dt = -thetaDot - (g / L) * sin(theta);  // Derivative of angular velocity
+    float dThetaDot_dt = - mu * thetaDot - (g / L) * sin(theta);  
 
-    // Output the new derivatives (dTheta/dt, dThetaDot/dt)
     gl_FragColor = vec4(dTheta_dt, dThetaDot_dt, 0.0, 1.0);
 }
